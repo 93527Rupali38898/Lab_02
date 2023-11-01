c6e0c958c7e87d408d50e1a82423afcb31afb6ba
93527Rupali38898 <143178726+93527Rupali38898@users.noreply.github.com>
2023-11-01 19:12:27 +0530

    Add files via upload

---
+++ Q10.py
@@ -0,0 +1,12 @@
+n = int(input("enter time in second:"))
+if n <= 86400:
+    x = n/3600
+    a = int(x) #hours
+    y = n - (a*3600)
+    b = int(y/60) #mins
+    c = y - (b*60) #secs
+    print(a,"hours", b, "mins",c, "seconds")
+else:
+    print('invalid input')
+
+
